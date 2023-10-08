b494c111062b8b7c73902ba83d1bda5a4d75446f
Yevhen-L <ujeen2101@gmail.com>
2023-10-08 12:40:53 +0300

    correct

--- src/components/friendslist/friendList.jsx
+++ src/components/friendslist/friendList.jsx
@@ -1,16 +1,5 @@
+import { FriendListItem } from './friendListItem';
 import css from './friends.module.css';
-
-const FriendListItem = ({ avatar, name, isOnline }) => {
-  return (
-    <li className={css.listitem}>
-      <span
-        className={`${css.status} ${isOnline ? css.online : css.offline}`}
-      ></span>
-      <img className={css.avatar} src={avatar} alt={name} width="48" />
-      <p className={css.name}>{name}</p>
-    </li>
-  );
-};
 
 export const FriendList = ({ friends }) => {
   return (
